4cc01e0f8ea20a8d848d4e6169e859ef1e121af6
Martin Thaller <thaller.martin@gmail.com>
2022-06-22 11:37:02 +0200

    adapt-navbar

--- src/Client/src/router/router.ts
+++ src/Client/src/router/router.ts
@@ -6,11 +6,13 @@ import settings from "../views/settings.vue";
 import welcome from "../views/welcome.vue";
 import home from "../views/home.vue";
 
+export const sharedCalendarRoute = "sharedCalendar"
+
 const routes = [
     { path: "/calendar", component: calendar, beforeEnter: Auth0.routeGuard },
     { path: "/settings", component: settings, beforeEnter: Auth0.routeGuard },
     { path: "/welcome", component: welcome },
-    { path: "/s/:id", component: sharedCalendar},
+    { path: "/s/:id", component: sharedCalendar, name: sharedCalendarRoute},
     { path: "/", component: home },
 ];
 
