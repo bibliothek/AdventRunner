3415d068e09d5c8a399dc0eb748435c427182389
Martin Thaller <thaller.martin@gmail.com>
2022-06-26 23:24:17 +0200

    spinner

--- src/Client/src/App.vue
+++ src/Client/src/App.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import Navbar from './components/navbar.vue';
+import Shell from './components/Shell.vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 </script>
 
 <template>
-  <div class="container m-auto p-4 bg-gray-100">
-    <div class="bg-white p-4 flex flex-col">
-      <Navbar></Navbar>
-      <router-view></router-view>
-    </div>
-  </div>
+    <Shell></Shell>
 </template>
 
