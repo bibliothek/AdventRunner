ce37846805373bc8527e0da79be6824606fe2fe8
Martin Thaller <thaller.martin@gmail.com>
2022-06-29 23:51:51 +0200

    type

--- src/Client/src/components/RunProgress.vue
+++ src/Client/src/components/RunProgress.vue
@@ -1,41 +1,41 @@
 <template>
-<div class="h-4 rounded-box flex flex-row">
+    <div class="h-4 rounded-box flex flex-row">
 
-    <div class="bg-primary rounded-l-lg" style="height:100%" :style="doneWidth"></div>
-    <div class="bg-warning" style="33%; height:100%" :style="openWidth"></div>
-    <div class="bg-neutral rounded-r-lg" style="33%; height:100%" :style="closedWidth"></div>
-</div>
+        <div class="bg-primary rounded-l-lg" style="height:100%" :style="doneWidth"></div>
+        <div class="bg-warning" style="33%; height:100%" :style="openWidth"></div>
+        <div class="bg-neutral rounded-r-lg" style="33%; height:100%" :style="closedWidth"></div>
+    </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import {Calendar, emptyCalendar, Door, DoorState, DoorStateCase} from "../models/calendar"
+import { Calendar, emptyCalendar, Door, DoorState, DoorStateCase } from "../models/calendar"
 
 let getTotal = (cal: Calendar) => {
-    return cal.doors.reduce((val, el) =>  val + el.distance, 0)
+    return cal.doors.reduce((val, el) => val + el.distance, 0)
 }
 
 let getByState = (cal: Calendar, state: DoorStateCase) => {
     return cal.doors.reduce((val, el) => el.state.case === state ? val + el.distance : val, 0)
 }
 
+let getWidthPropertyForState = (cal: Calendar, state: DoorStateCase) => {
+    const percent = (getByState(cal as Calendar, state) / getTotal(cal as Calendar)) * 100;
+    return `width: ${percent}%`;
+}
+
 export default defineComponent({
     name: "RunProgressComponent",
     props: {
-        cal: {}
+        cal: Object as () => Calendar
     },
     computed: {
         doneWidth() {
-            const percentageDone = (getByState(this.cal as Calendar, "Done") / getTotal(this.cal as Calendar)) * 100;
-            console.log(percentageDone)
-            return `width: ${percentageDone}%`;
+            return getWidthPropertyForState(this.cal!, "Done");
         },
-        openWidth() {
-            const percentageDone = (getByState(this.cal as Calendar, "Open") / getTotal(this.cal as Calendar)) * 100;
-            return `width: ${percentageDone}%`;
+        openWidth() {return getWidthPropertyForState(this.cal!, "Open");
         },
         closedWidth() {
-            const percentageDone = (getByState(this.cal as Calendar, "Closed") / getTotal(this.cal as Calendar)) * 100;
-            return `width: ${percentageDone}%`;
+            return getWidthPropertyForState(this.cal!, "Closed");
         },
     }
 })
