38827ae000891a10a18e17e4386d083970ff8ce0
Martin Thaller <thaller.martin@gmail.com>
2022-07-29 11:42:51 +0200

    use new component

--- src/Client/src/views/sharedCalendar.vue
+++ src/Client/src/views/sharedCalendar.vue
@@ -5,16 +5,7 @@
                 <div class="text-2xl font-semibold text-content">{{ displayName }}</div>
                 <div class="text-md font-light">{{ sharedLinkResponse.period }}</div>
             </div>
-            <RunProgress :cal="sharedLinkResponse.calendar"></RunProgress>
-            <div class="flex flex-row flex-wrap max-w-6xl justify-center">
-                <div class="w-auto" v-for="door in sharedLinkResponse.calendar.doors" :key="door.day">
-                    <ClosedDoor v-if="door.state.case === 'Closed'" :day="door.day" />
-                    <OpenDoor v-if="door.state.case === 'Open'" :day="door.day" :isDone="false"
-                        :distance="distanceFor(door)" />
-                    <OpenDoor v-if="door.state.case === 'Done'" :day="door.day" :isDone="true"
-                        :distance="distanceFor(door)" />
-                </div>
-            </div>
+            <DoorCalendar :cal="sharedLinkResponse.calendar" :readonly="true"></DoorCalendar>
         </div>
         <div class="h-96 flex items-center" v-else>
             <div class="flex flex-col justify-center items-center">
@@ -29,6 +20,7 @@
 import { Door } from "../models/calendar";
 
 import { defineComponent } from "@vue/runtime-core";
+import DoorCalendar from "../components/DoorCalendar/DoorCalendar.vue";
 import ClosedDoor from "../components/DoorCalendar/ClosedDoor.vue";
 import OpenDoor from "../components/DoorCalendar/OpenDoor.vue";
 import RunProgress from "../components/RunProgress.vue";
@@ -38,7 +30,7 @@ import { sharedCalendarRoute } from "../router/router";
 
 export default defineComponent({
     name: "SharedCalendarComponent",
-    components: { ClosedDoor, OpenDoor, RunProgress },
+    components: { ClosedDoor, OpenDoor, RunProgress, DoorCalendar },
     computed: {
         hasResponse() {
             return isSome(this.$store.state.currentSharedCalendar[1]);
